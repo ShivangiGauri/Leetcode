568eae9f9b621a22b817e1a2fba4bbc3895ff0d0
Shivangi Singh <gaurishivangi2005@gmail.com>
2026-08-19 10:21:18 +0530

    Time: 3 ms (94.52%) | Memory: 22.9 MB (56.42%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.cpp
+++ 907-koko-eating-bananas/koko-eating-bananas.cpp
@@ -1,36 +1,27 @@
-class Solution { 
-public: 
-    bool isPossible(vector<int>& piles, int h, int mid) { 
-        long long int count = 0; 
-        for(auto it : piles) { 
-            if(mid > it) 
-                count += 1; 
-            else { 
-                if(it % mid == 0) 
-                    count += (it / mid); 
-                else 
-                    count += (it / mid) + 1; 
-            } 
-        } 
-        return count <= h; 
-    } 
-
-    int minEatingSpeed(vector<int>& piles, int h) { 
-        int maxi = INT_MIN; 
-        for(auto it : piles) 
-            maxi = max(maxi, it); 
-        int low = 1, high = maxi; 
-        int ans = -1; 
-        while(low <= high) { 
-            int mid = low + (high - low) / 2; 
-            if(isPossible(piles, h, mid)) { 
-                ans = mid; 
-                high = mid - 1; 
-            } 
-            else { 
-                low = mid + 1; 
-            } 
-        } 
-        return ans; 
-    } 
+class Solution {
+public:
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int res = -1;
+        int high = 1;
+        for(int i = 0; i < piles.size(); i++) {
+            high = max(high, piles[i]);
+        }
+        int low = 1;
+        while(low <= high) {
+            int mid = (low + high) / 2;
+            long long count = 0;
+            for(int i = 0; i < piles.size(); i++) {
+                    count += piles[i] / mid;
+                if(piles[i]%mid !=0)
+                    count += 1;
+            }
+            if(count > h)
+                low = mid + 1;
+            else {
+                res = mid;
+                high = mid - 1;
+            }
+        }
+        return res;
+    }
 };
